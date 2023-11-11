a89d15aec0003ca1d22f04778c1980a0a067a41b
bk.sambhaw <bk.sambhaw@outlook.com>
2023-11-11 22:33:57 +0530

    mth

---
+++ JS_codeSpace/2023-11-11__numMethods.js
@@ -0,0 +1,30 @@
+const score = 357
+
+const teamScore = new Number(321)
+
+// console.log(teamScore.toString().length);
+// console.log(teamScore.toFixed(2));  //* To get the precision value.(generally used in eCommerceApps)
+
+const anotherNum = 17.86242514
+// console.log(anotherNum.toPrecision(2));  //* 18
+// console.log(anotherNum.toPrecision(3));  //* 17.9
+// console.log(anotherNum.toPrecision(4));  //* 17.86
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));  //* 10,00,000
+
+
+// Math=======================================================================================
+
+console.log(Math);
+console.log(Math.abs(-4));  //* 4
+console.log(Math.round(4.7));   //* 5
+console.log(Math.ceil(4.2));    //* 5, 
+console.log(Math.floor(4.2));    //* 4, 
+
+
+
+
+
+
