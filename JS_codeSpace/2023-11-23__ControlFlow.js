a948de989753dd27b4362ab2945353399301a6d8
bk.sambhaw <bk.sambhaw@outlook.com>
2023-12-23 23:44:30 +0530

    controlFlow

--- JS_codeSpace/2023-11-23__ControlFlow.js
+++ JS_codeSpace/2023-11-23__ControlFlow.js
@@ -1,6 +1,6 @@
 //. if_Statement
 // if (condition) {
-//     // ifcondition is true, only then this gets executed 
+//     // condition = true, only then this gets executed 
 // }
 
 //. if-else
@@ -11,21 +11,76 @@
 // }
 
 //- conditional operators
-//- <, >, <=, >=, ==, !=, ===, !==
+//- <, >, <=, >=, ==, !=, ===(checks for datatype & value too), !==
 
 const score = 200;
-if(score >100){
-    // var power = "fly"
+if (score > 100) {
+    // var power = "fly"    //* var, is a global
     // let power = "Wind" //* let, prints the console within the {} / scope.
-    // const power = "spark"   //* const, prints the console within the {} / scope.
+    const power = "spark"   //* const, prints the console within the {} / scope.
     console.log(`My Power is ${power}`);
 }
-console.log(`My Power is ${power}`); //* if var, it prints the value. Hence there is a chance of variable pollution.
+// console.log(`My Power is ${power}`); //* if var, it prints the value. Hence there is a chance of variable pollution.
+//* gets undefined for let & const.
+
+//. if_else ShortHandNotation(SHN)
+const balance = 1000;
+if (balance > 500) console.log("test--");
 
 //- ===============================================================================================================================
 //- ===============================================================================================================================
 
 
+//. Nesting
+const bal = 1000;
+if (bal < 500) {
+    console.log(`less than 500`);
+}else if(bal <700){
+    console.log(`less than 700`);
+}else if(bal<900){
+    console.log(`less than 900`);
+}
+console.log(`less than 1200`);
 
+let userLoggedIn = true;
+const userLoggedInusingEmail = true;
+let debitCard = true
+
+//- &&_And operator
+if(userLoggedIn && debitCard){
+    console.log(`Allow to buy course`);
+}
+//- ||_or operator
+if(userLoggedIn || userLoggedInusingEmail){
+    console.log(`User Logged In`);
+}
+
+//. Switch
+const weekEnd = "Saturday"
+switch (weekEnd) {
+    case "Sunday":
+        console.log("WeekEnd");
+        break;
+    case "Monday":
+        console.log("WeekDay");
+        break;
+    case "Tuesday":
+        console.log("WeekDay");
+        break;
+    case "Wednesday":
+        console.log("WeekDay");
+        break;
+    case "Thursday":
+        console.log("WeekDay");
+        break;
+    case "Friday":
+        console.log("WeekDay");
+        break;
+    case "Saturday":
+        console.log("WeekEnd");
+        break;
+    default:
+        break;
+}
 
 
